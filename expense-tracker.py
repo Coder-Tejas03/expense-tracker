7529408e788822fdfa1248128ea9ae286d418610
Coder-Tejas03 <tejasgosavi8321@gmail.com>
2026-08-20 16:11:50 +0530

    Summary and get specific expense endpoints added

--- expense-tracker.py
+++ expense-tracker.py
@@ -17,7 +17,7 @@
 # 6. Write GET /expenses
 #    → returns the full expenses_db list
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -34,10 +34,24 @@ def add_expense(expense: Expense):
     expense_data = expense.model_dump()
     expense_data["id"] = len(expenses_db) + 1
     expenses_db.append(expense_data)
-    return {"message": "Added!", "data": expense}
+    return {"message": "Added!", "data": expense_data}
     
 @app.get("/expenses")
 def get_expenses():
     return expenses_db
 
+@app.get("/expenses/summary")
+def get_summary():
+    summary = {}
+    for expense in expenses_db:
+        category = expense["category"]
+        summary[category] = summary.get(category, 0) + expense["amount"]
+    
+    return summary
 
+@app.get("/expenses/{expense_id}")
+def get_expense(expense_id: int):
+    for expense in expenses_db:
+        if expense['id'] == expense_id:
+            return expense
+    raise HTTPException(status_code=404, detail="Expense not found!")
